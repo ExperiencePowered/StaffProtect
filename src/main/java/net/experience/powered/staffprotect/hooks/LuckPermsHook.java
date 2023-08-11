d6a283b62b67650657ff490246d5f546b8643c0c
Jakub Šulc <James_Sulc@outlook.cz>
2023-08-11 20:50:32 +0200

    Added a listener when permissions for user are recalculated, it will remove that user from subscribers, and in case the player still has permission for notifications, we add him again

--- src/main/java/net/experience/powered/staffprotect/hooks/LuckPermsHook.java
+++ src/main/java/net/experience/powered/staffprotect/hooks/LuckPermsHook.java
@@ -1,12 +1,16 @@
 package net.experience.powered.staffprotect.hooks;
 
+import net.experience.powered.staffprotect.StaffProtect;
+import net.experience.powered.staffprotect.StaffProtectAPI;
 import net.experience.powered.staffprotect.interfaces.Permission;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.cacheddata.CachedPermissionData;
+import net.luckperms.api.event.user.UserDataRecalculateEvent;
 import net.luckperms.api.model.user.User;
 import net.luckperms.api.model.user.UserManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
@@ -23,6 +27,7 @@ public class LuckPermsHook implements Permission {
         } else {
             throw new IllegalStateException("LuckPerms is null, yet it was initialised");
         }
+        luckPerms.getEventBus().subscribe(JavaPlugin.getPlugin(StaffProtect.class), UserDataRecalculateEvent.class, this::UserDataRecalculate);
     }
 
     @Override
@@ -35,5 +40,14 @@ public class LuckPermsHook implements Permission {
         }
         final CachedPermissionData cachedData = user.getCachedData().getPermissionData();
         return cachedData.checkPermission(permission).asBoolean();
+    }
+
+    private void UserDataRecalculate(final @NotNull UserDataRecalculateEvent e) {
+        final var uuid = e.getUser().getUniqueId();
+        final var notificationBus = StaffProtectAPI.getInstance().getNotificationBus();
+        notificationBus.unsubscribe(uuid);
+        if (hasPermission(uuid, "staffprotect.notification")) {
+            notificationBus.subscribe(uuid);
+        }
     }
 }
