49a6ac4be066fd558c1f9116046ba766c28b8faa
Jakub Šulc <James_Sulc@outlook.cz>
2023-08-12 13:11:26 +0200

    Using CommandMap from MorePaperLib

--- API/src/main/java/net/experience/powered/staffprotect/util/CommandRegisterer.java
+++ API/src/main/java/net/experience/powered/staffprotect/util/CommandRegisterer.java
@@ -1,10 +1,12 @@
 package net.experience.powered.staffprotect.util;
 
+import net.experience.powered.staffprotect.StaffProtectAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandMap;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import space.arim.morepaperlib.MorePaperLib;
 
 import java.lang.reflect.Field;
 
@@ -13,25 +15,21 @@ public class CommandRegisterer {
     private CommandMap commandMap;
 
     private CommandRegisterer(final @NotNull String bukkitName) {
+        final var api = StaffProtectAPI.getInstance();
+        final var paperLib = new MorePaperLib(api.getPlugin());
         switch (bukkitName) {
-            case "Paper" -> this.commandMap = Bukkit.getCommandMap();
-            case "Spigot" -> { // Added for backwards compatibility for Spigot
-                Field fCommandMap;
+            // Changed with MorePaperLib api which ensures backwards compatibility
+            case "Paper", "Spigot" -> commandMap = paperLib.commandRegistration().getServerCommandMap();
+            default -> {
+                Bukkit.getLogger().warning("You're running on unsupported version.");
                 try {
+                    Field fCommandMap;
                     fCommandMap = Bukkit.getPluginManager().getClass().getDeclaredField("commandMap");
                     fCommandMap.setAccessible(true);
                     final Object commandMapObject = fCommandMap.get(Bukkit.getPluginManager());
                     if (commandMapObject instanceof CommandMap) {
                         this.commandMap = (CommandMap) commandMapObject;
                     }
-                } catch (final NoSuchFieldException | IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            default -> {
-                Bukkit.getLogger().warning("You're running on unsupported version.");
-                try {
-                    this.commandMap = Bukkit.getCommandMap();
                 } catch (final Exception e) {
                     new CommandRegisterer("Spigot");
                 }
