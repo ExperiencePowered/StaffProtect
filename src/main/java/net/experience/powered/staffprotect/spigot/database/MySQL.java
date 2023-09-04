d6dfe1bad099da2e9b8f84fd11d8761267f04f19
Jakub Šulc <James_Sulc@outlook.cz>
2023-09-04 13:46:11 +0200

    Implemented functions in MySQL

--- src/main/java/net/experience/powered/staffprotect/spigot/database/MySQL.java
+++ src/main/java/net/experience/powered/staffprotect/spigot/database/MySQL.java
@@ -5,6 +5,8 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public final class MySQL extends AbstractDatabase {
     private HikariDataSource hikari;
@@ -15,21 +17,41 @@ public final class MySQL extends AbstractDatabase {
 
     @Override
     public void connect() {
+        hikari = new HikariDataSource(hikariConfig);
 
+        try {
+            assert getConnection() != null;
+            PreparedStatement statement = getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS verification (playerName varchar(255), secretKey varchar(31))");
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void disconnect() {
-
+        if (this.isConnected()) {
+            try {
+                assert getConnection() != null;
+                getConnection().close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            this.hikari = null;
+        }
     }
 
     @Override
     public boolean isConnected() {
-        return false;
+        return hikari != null;
     }
 
     @Override
     public Connection getConnection() {
-        return null;
+        try {
+            return hikari.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
