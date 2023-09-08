47bf6cfa7c5cdf7fb4ba5ce668fcea9c9b715abd
Jakub Šulc <James_Sulc@outlook.cz>
2023-09-08 16:54:23 +0200

    Removed abstracting of disconnect, isConnected and getConnection method, as these can be same because of using hikari data source

--- src/main/java/net/experience/powered/staffprotect/spigot/database/AbstractDatabase.java
+++ src/main/java/net/experience/powered/staffprotect/spigot/database/AbstractDatabase.java
@@ -1,22 +1,38 @@
 package net.experience.powered.staffprotect.spigot.database;
 
-import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public abstract class AbstractDatabase {
 
-    protected final HikariConfig hikariConfig;
+    protected HikariDataSource dataSource;
+    protected final DatabaseProperties properties;
 
-    public AbstractDatabase(final @NotNull HikariConfig hikariConfig) {
-        this.hikariConfig = hikariConfig;
+    public AbstractDatabase(final @NotNull DatabaseProperties properties) {
+        this.properties = properties;
     }
 
     public abstract void connect();
-    public abstract void disconnect();
-    public abstract boolean isConnected();
-    public abstract Connection getConnection();
-
+    public void disconnect() {
+        if (this.isConnected()) {
+            this.dataSource.close();
+            this.dataSource = null;
+        }
+    }
+    public boolean isConnected() {
+        return this.dataSource != null;
+    }
+    public Connection getConnection() {
+        if (!isConnected()) {
+            connect();
+        }
+        try {
+            return this.dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
