2c24a2f4acb50ed6f0da3de6c24eb8063840ed01
Jakub Šulc <James_Sulc@outlook.cz>
2023-09-08 16:55:05 +0200

    Removed unnecessary methods which were written directly to AbstractDatabase

--- src/main/java/net/experience/powered/staffprotect/spigot/database/SQLite.java
+++ src/main/java/net/experience/powered/staffprotect/spigot/database/SQLite.java
@@ -1,20 +1,18 @@
 package net.experience.powered.staffprotect.spigot.database;
 
 import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public final class SQLite extends AbstractDatabase {
-    private Connection connection;
     private final File file;
 
-    public SQLite(final @NotNull HikariConfig hikariConfig, final @NotNull File file) {
-        super(hikariConfig);
+    public SQLite(final @NotNull DatabaseProperties properties, final @NotNull File file) {
+        super(properties);
         this.file = file;
     }
 
@@ -24,39 +22,16 @@ public final class SQLite extends AbstractDatabase {
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
-        try {
-            connection = DriverManager.getConnection("jdbc:sqlite:" + this.file);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+
+        final HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:sqlite:" + this.file);
+        dataSource = new HikariDataSource(config);
 
         try {
-            PreparedStatement statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS verification (playerName varchar(255), secretKey varchar(31))");
+            PreparedStatement statement = getConnection().prepareStatement("CREATE TABLE IF NOT EXISTS verification (playerName varchar(255), secretKey varchar(31))");
             statement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    public void disconnect() {
-        if (this.isConnected()) {
-            try {
-                this.connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            this.connection = null;
-        }
-    }
-
-    public boolean isConnected() {
-        return this.connection != null;
-    }
-
-    public Connection getConnection() {
-        if (!isConnected()) {
-            connect();
-        }
-        return this.connection;
     }
 }
