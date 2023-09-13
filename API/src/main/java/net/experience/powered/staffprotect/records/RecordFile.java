0692a917f9db833f41150bfd2da1712423d1f88b
Jakub Šulc <James_Sulc@outlook.cz>
2023-09-13 17:33:58 +0200

    Added static field for dateformat, changed return type and all methods body for readRecords

--- API/src/main/java/net/experience/powered/staffprotect/records/RecordFile.java
+++ API/src/main/java/net/experience/powered/staffprotect/records/RecordFile.java
@@ -1,20 +1,32 @@
 package net.experience.powered.staffprotect.records;
 
 import net.experience.powered.staffprotect.StaffProtect;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class RecordFile {
 
     private static RecordFile instance;
 
+    public static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
     public static final File folder;
 
     private final File file;
-    private final List<Record> records;
 
     static {
         folder = new File(StaffProtect.getInstance().getPlugin().getDataFolder(), "records");
@@ -28,24 +40,78 @@ public class RecordFile {
 
     public RecordFile(final @NotNull File file) {
         this.file = file;
-        this.records = new ArrayList<>();
-
         RecordFile.instance = this;
     }
 
     public void writeRecord(final @NotNull Record record) {
         record.write(file);
-        records.add(record);
     }
 
-    public List<Record> readRecords(final @NotNull String player) {
-        List<Record> list = new ArrayList<>();
-        records.forEach(record -> {
-            if (record.getPlayer().equalsIgnoreCase(player)) {
-                list.add(record);
+    public CompletableFuture<List<CapturedRecordFile>> readRecords(final @NotNull Player player) {
+        final Pattern pattern = Pattern.compile("\\[(.*?)\\]\\:");
+        return CompletableFuture.supplyAsync(() -> {
+            final List<CapturedRecordFile> captures = new ArrayList<>();
+            final File[] files = folder.listFiles();
+            if (files == null) {
+                return captures;
             }
+            for(File file1 : files) {
+                if (file1.isDirectory()) {
+                    break;
+                }
+                try (BufferedReader reader = new BufferedReader(new FileReader(file1))) {
+                    final CapturedRecordFile capture = new CapturedRecordFile(file1);
+                    Stream<String> stream = reader.lines();
+                    stream.forEach(line -> {
+                        long time;
+                        String content;
+                        String[] stringArray = line.split(" ");
+                        Matcher matcher = pattern.matcher(line);
+                        if (matcher.find()) {
+                            content = line.substring(matcher.start() + stringArray[1].length() + 1);
+                        }
+                        else {
+                            content = null;
+                        }
+
+                        String array1 = stringArray[0];
+                        if (array1.startsWith("(") && array1.endsWith(")")) {
+                            array1 = array1.replace("(", "")
+                                    .replace(")", "");
+                            try {
+                                Date date = dateFormat.parse(array1);
+                                time = date.getTime();
+                            } catch (NumberFormatException | ParseException e) {
+                                time = -1;
+                            }
+                        }
+                        else {
+                            time = -1;
+                        }
+                        
+                        String array2 = stringArray[1];
+                        if (array2.startsWith("[") && array2.endsWith("]:")) {
+                            array2 = array2.replace("[", "")
+                                    .replace("]:", "");
+                            if (player.getName().equalsIgnoreCase(array2)) {
+                                capture.getRecords().add(new Record(time, player.getName(), content));
+                            }
+                            InetSocketAddress address = player.getAddress();
+                            if (address != null & (address.getHostString() + ":" + address.getPort()).equals(array2)) {
+                                capture.getRecords().add(new Record(time, player.getName(), content));
+                            }
+                        }
+                    });
+                    captures.add(capture);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return captures;
+        }).exceptionally(throwable -> {
+            throwable.printStackTrace();
+            return List.of();
         });
-        return list;
     }
 
     public static RecordFile getInstance() {
