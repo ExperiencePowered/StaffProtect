1d4869c55b625303a033bd6f94e61531631de3a8
Jakub Šulc <James_Sulc@outlook.cz>
2023-09-23 18:51:24 +0200

    Added ActionType in method

--- API/src/main/java/net/experience/powered/staffprotect/notification/NotificationManager.java
+++ API/src/main/java/net/experience/powered/staffprotect/notification/NotificationManager.java
@@ -1,9 +1,12 @@
 package net.experience.powered.staffprotect.notification;
 
+import net.experience.powered.staffprotect.records.ActionType;
 import net.kyori.adventure.text.Component;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
 
 public abstract class NotificationManager {
 
@@ -20,18 +23,21 @@ public abstract class NotificationManager {
     }
 
     /**
-     * Sends a message to all subscribers ({@link NotificationBus#getSubscribers()})
-     * @param player player which caused notification to happen, can be null, although it is not good to do and may be changed in the future
+     * Sends a message to all subscribers ({@link NotificationBus#getModernSubscribers()})
+     * @param player player which caused notification to happen
+     * @param uuid player's uuid which caused notification
+     * @param actionType type of action which happened
      * @param component component which contains the message
      */
-    public abstract void sendMessage(final @Nullable String player, final @NotNull Component component);
+    public abstract void sendMessage(final @NotNull String player, final @NotNull UUID uuid, final @NotNull Component component, final @NotNull ActionType actionType);
 
     /**
      * Sends a message to record file
      * @param player player which caused notification to happen, can be null, although it is not good to do and may be changed in the future
      * @param string message
+     * @param actionType type of action which happened
      */
-    public abstract void sendQuietMessage(final @Nullable String player, final @NotNull String string);
+    public abstract void sendQuietMessage(final @Nullable String player, final @NotNull String string, final @NotNull ActionType actionType);
 
     /**
      * Gets public instance of this class
