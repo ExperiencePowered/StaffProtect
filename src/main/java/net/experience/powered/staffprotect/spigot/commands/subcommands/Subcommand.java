072d33c1f9025d7a87b6fe2e0d431e5597039087
Jakub Šulc <James_Sulc@outlook.cz>
2023-09-23 18:52:09 +0200

    Added commented code

--- src/main/java/net/experience/powered/staffprotect/spigot/commands/subcommands/Subcommand.java
+++ src/main/java/net/experience/powered/staffprotect/spigot/commands/subcommands/Subcommand.java
@@ -5,6 +5,13 @@ import org.jetbrains.annotations.NotNull;
 
 public abstract class Subcommand {
 
+    // Will be added in future, for now is commented as there is no use for it
+    //private final String label;
+
+    //public Subcommand(final @NotNull String label) {
+    //    this.label = label;
+    //}
+
     public final void register() {
         SubcommandManager.register(getClass(), this);
     }
@@ -12,6 +19,10 @@ public abstract class Subcommand {
     public final void unregister() {
         SubcommandManager.unregister(getClass());
     }
+
+    //public final String getLabel() {
+    //    return label;
+    //}
 
     public abstract boolean command(final @NotNull CommandSender sender, final @NotNull String[] args);
 
